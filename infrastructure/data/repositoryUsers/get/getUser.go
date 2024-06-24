347727684fc68e4dab5e1eb00023f1a441cbe14e
Franco Rutigliano <francorutigliano11@gmail.com>
2024-06-24 17:54:18 -0300

    repository users http method GET inicialized

---
+++ infrastructure/data/repositoryUsers/get/getUser.go
@@ -0,0 +1,8 @@
+package get
+
+import users "GoEcom/pkg/domain"
+
+func (UserGet) GetUsers() users.CustomerInfo {
+
+	return users.CustomerInfo{}
+}
--- infrastructure/data/repositoryUsers/get/init.go
+++ infrastructure/data/repositoryUsers/get/init.go
@@ -1,6 +1,9 @@
 package get
 
+import users "GoEcom/pkg/domain"
+
 type IUserGet interface {
+	GetUsers() users.CustomerInfo
 }
 
 type UserGet struct {
