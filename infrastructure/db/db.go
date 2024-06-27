4e2314689e5e929519b9deee0d8aecf3803a96c7
Franco Rutigliano <francorutigliano11@gmail.com>
2024-06-27 19:06:47 -0300

    func to ping the db

--- infrastructure/db/db.go
+++ infrastructure/db/db.go
@@ -3,14 +3,12 @@ package db
 import (
 	"database/sql"
 	"log"
-
-	"github.com/go-sql-driver/mysql"
 )
 
-func NewSqlStorage(cfg mysql.Config) (*sql.DB, error) {
-	db, err := sql.Open("mysql", cfg.FormatDSN())
-	if err != nil {
+func InitStorage(db *sql.DB) {
+	if err := db.Ping(); err != nil {
 		log.Fatal(err)
 	}
-	return db, err
+
+	log.Println("Database connected")
 }
